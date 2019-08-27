9b4d985f82e231b0e43913e97e3aba1a57bba04f
Rolands Usans <rolands.usans@gmail.com>
2019-08-27 23:36:42 +0300

    chmod + echo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-sh -c "echo Hello world, this is executet from Docker container. ENV: $ENVIRONMENT"
+sh -c "echo Hello world, this is executet from Docker container. Variable ENV: $ENVIRONMENT"
